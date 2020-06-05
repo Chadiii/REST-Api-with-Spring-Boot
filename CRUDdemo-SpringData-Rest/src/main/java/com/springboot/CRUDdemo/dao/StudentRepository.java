75a7abb6d3e4de5c0cfefdbf44fabf4524aae8af
Chadiii <chadilaoulaou@gmail.com>
2020-06-05 17:19:03 +0000

    Adding Spring Data Rest Support

---
+++ CRUDdemo-SpringData-Rest/src/main/java/com/springboot/CRUDdemo/dao/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.CRUDdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.springboot.CRUDdemo.entity.Student;
+
+@RepositoryRestResource(path="members")
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+	//no need to add anycode
+}
